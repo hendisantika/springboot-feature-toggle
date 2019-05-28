545c3fafd74a6a589f3a6ffcacd4d1e94a9fe768
hendisantika <hendisantika@yahoo.co.id>
2019-05-28 22:28:15 +0700

    #10 - Add checkFeatureToggle method in DebugController

--- src/main/java/com/hendisantika/springbootfeaturetoggle/controller/DebugController.java
+++ src/main/java/com/hendisantika/springbootfeaturetoggle/controller/DebugController.java
@@ -1,8 +1,13 @@
 package com.hendisantika.springbootfeaturetoggle.controller;
 
+import com.hendisantika.springbootfeaturetoggle.config.AvailableFeatures;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.togglz.core.manager.FeatureManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +26,13 @@ public class DebugController {
 
     public DebugController(FeatureManager featureManager) {
         this.featureManager = featureManager;
+    }
+
+    @GetMapping("/checkFeature")
+    public Map<String, Object> checkFeatureToggle() {
+        return new HashMap<String, Object>() {{
+            put("(1) is the secret feature enabled?", featureManager.isActive(AvailableFeatures.ENABLE_SECRET_FEATURE));
+            put("(2) is the secret feature enabled?", AvailableFeatures.ENABLE_SECRET_FEATURE.isActive());
+        }};
     }
 }
