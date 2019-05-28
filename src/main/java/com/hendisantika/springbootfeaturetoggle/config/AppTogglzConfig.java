234018c7348a2b427ef79c50a50371d92e18571c
hendisantika <hendisantika@yahoo.co.id>
2019-05-28 22:27:00 +0700

    #8 - Add another Method in AppTogglzConfig

--- src/main/java/com/hendisantika/springbootfeaturetoggle/config/AppTogglzConfig.java
+++ src/main/java/com/hendisantika/springbootfeaturetoggle/config/AppTogglzConfig.java
@@ -3,11 +3,19 @@ package com.hendisantika.springbootfeaturetoggle.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.togglz.core.manager.EnumBasedFeatureProvider;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.spi.FeatureProvider;
+import org.togglz.core.user.FeatureUser;
+import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
 import redis.clients.jedis.JedisPool;
+
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,4 +57,32 @@ public class AppTogglzConfig {
         return new EnumBasedFeatureProvider(AvailableFeatures.class);
     }
 
+    private FeatureUser getCurrentUserProvider() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        if (authentication == null) {
+            return null;
+        }
+        // try to obtain the name of this user
+        String name = getName(authentication);
+        // check for the authority for feature admins
+        Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        boolean featureAdmin = isFeatureAdmin(authentication, authorities);
+
+        SimpleFeatureUser user = new SimpleFeatureUser(name, featureAdmin);
+        user.setAttribute(USER_ATTRIBUTE_ROLES, authorities);
+        return user;
+    }
+
+    private String getName(Authentication authentication) {
+        if (authentication.getPrincipal() == null) {
+            return "unknown_user";
+        }
+        return authentication.getPrincipal().toString();
+    }
+
+    private boolean isFeatureAdmin(Authentication authentication, Set<String> authorities) {
+//        return authorities.contains("SUPER_ADMIN");
+        return true;
+    }
 }
